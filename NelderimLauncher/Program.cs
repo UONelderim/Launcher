058199a241b087bc60da775a0c3afe274c1f6ce5
kaczy <kaczyf93@gmail.com>
2024-02-16 16:10:23 +0100

    New autoupdate aproach

--- NelderimLauncher/Program.cs
+++ NelderimLauncher/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Diagnostics;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Runtime.Loader;
 
@@ -41,12 +42,44 @@ namespace Nelderim.Launcher
         
         public static void Main(string[] args)
         {
+            if (args.Length == 2 && args[0] == "autoupdate")
+            {
+                var targetPath = args[1];
+                AutoUpdate(targetPath);
+                return;
+            }
             Console.WriteLine(RuntimeInformation.RuntimeIdentifier);
             _rootDir = AppContext.BaseDirectory;
             _osDir = GetOsIdentifier();
             _loadContext = AssemblyLoadContext.Default;
             _loadContext.ResolvingUnmanagedDll += ResolveUnmanagedDll;
-            using (var game = new NelderimLauncher()) game.Run();
+            using (var game = new NelderimLauncher(args)) game.Run();
+        }
+
+        public static void AutoUpdate(string targetPath)
+        {
+            try
+            {
+                var currentProcessPath = Environment.ProcessPath;
+                if (currentProcessPath != null)
+                {
+                    if (targetPath != null)
+                    {
+                        File.Copy(currentProcessPath, targetPath, true);
+                        var process = new Process();
+                        process.StartInfo.FileName = Path.GetFullPath(targetPath);
+                        process.StartInfo.Arguments = "Aktualizacja zakonczona pomyslnie";
+                        process.Start();
+                        File.Delete(currentProcessPath); //Will this even work?
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine("Press any key to continue");
+                Console.ReadKey();
+            }
         }
     }
 }
