e4600e399e46db2819b5d18ee9c73ae68da03917
kaczy <kaczyf93@gmail.com>
2024-02-18 23:44:02 +0100

    New SHA1 function

--- Nelderim/Utility/Crypto.cs
+++ Nelderim/Utility/Crypto.cs
@@ -1,5 +1,4 @@
 ﻿using System.Security.Cryptography;
-using System.Text;
 
 namespace Nelderim.Utility;
 
@@ -7,14 +6,6 @@ public class Crypto
 {
     public static string Sha1Hash(Stream stream)
     {
-        byte[] hash = SHA1.Create().ComputeHash(stream);
-        StringBuilder sb = new StringBuilder(hash.Length * 2);
-
-        foreach (byte b in hash)
-        {
-            sb.Append(b.ToString("X"));
-        }
-
-        return sb.ToString();
+        return Convert.ToHexString(SHA1.HashData(stream)).ToUpper();
     }
 }
