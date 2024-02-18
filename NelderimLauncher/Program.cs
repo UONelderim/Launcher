5e0c1ac736d6f2c60bdd01fea93d0294c9d0dc79
kaczy <kaczyf93@gmail.com>
2024-02-18 23:32:01 +0100

    Remove unused dll code

--- NelderimLauncher/Program.cs
+++ NelderimLauncher/Program.cs
@@ -7,39 +7,6 @@ namespace Nelderim.Launcher
 {
     public static class Program
     {
-        private static AssemblyLoadContext _loadContext;
-        private static string? _rootDir;
-        private static string _osDir = "";
-
-        private static string GetOsIdentifier()
-        {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                return "x64";
-            }
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            {
-                return "osx";
-            }
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                return "lib64";
-            }
-            return "unknown";
-        }
-        private static IntPtr ResolveUnmanagedDll(Assembly assembly, string unmanagedDllName)
-        {
-            var libraryPath = Path.Combine(_rootDir, _osDir, unmanagedDllName);
-
-            Console.WriteLine($"Loading unmanaged DLL  {libraryPath}");
-
-            if (File.Exists(libraryPath)) 
-                return NativeLibrary.Load(libraryPath);
-            
-            Console.WriteLine($"ERROR: {libraryPath} doesn't exist");
-            return IntPtr.Zero;
-        }
-        
         public static void Main(string[] args)
         {
             if (args.Length == 2 && args[0] == "autoupdate")
@@ -48,11 +15,6 @@ namespace Nelderim.Launcher
                 AutoUpdate(targetPath);
                 return;
             }
-            Console.WriteLine(RuntimeInformation.RuntimeIdentifier);
-            _rootDir = AppContext.BaseDirectory;
-            _osDir = GetOsIdentifier();
-            _loadContext = AssemblyLoadContext.Default;
-            _loadContext.ResolvingUnmanagedDll += ResolveUnmanagedDll;
             using (var game = new NelderimLauncher(args)) game.Run();
         }
 
